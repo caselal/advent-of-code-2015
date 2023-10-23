ccf35629e9442889ca2aa86e644a87006530a358
Cassandra Luong <cassandraluong@gmail.com>
2023-10-23 05:09:03 -0400

    Complete AoC Day 5 - optimize Part Two

--- 05-doesnt-he-have-intern-elves-for-this/doesnt_he_have_intern_elves_for_this.py
+++ 05-doesnt-he-have-intern-elves-for-this/doesnt_he_have_intern_elves_for_this.py
@@ -36,27 +36,9 @@ for s in strings_list:
 
 print(strings_nice)
 
-
 ###########
 # Part Two
 ###########
-def find_indexes(string, substring):
-    start = 0
-    indexes = []
-
-    while start < len(string):
-        start = string.find(substring, start)
-
-        if start == -1:
-            return indexes
-
-        indexes.append(start)
-
-        start += len(substring)
-
-    return indexes
-
-
 strings_nice = 0
 
 for s in strings_list:
@@ -64,17 +46,15 @@ for s in strings_list:
     count_one_letter_between = 0
 
     # Identify strings containing pairs of any two letters that appear at least twice in the string without overlapping
-    all_substrings_count = {}
+    all_substrings = {}
 
-    all_substrings = [s[i:i + 2] for i in range(len(s) - 1)]
-    all_substrings_unique = set(all_substrings)
-    all_substrings_unique = list(all_substrings_unique)
-
-    for substring in all_substrings_unique:
-        if len(find_indexes(s, substring)) > 1:
-            all_substrings_count[substring] = 1
-
-    count_two_letters = len(all_substrings_count)
+    for i in range(len(s) - 1):
+        substring = s[i] + s[i + 1]
+        if all_substrings.get(substring) is None:
+            all_substrings[substring] = i
+        else:
+            if all_substrings.get(substring) != i - 1:
+                count_two_letters += 1
 
     # Identify strings containing at least one letter which repeats with exactly one letter between them
     for i in range(len(s) - 2):

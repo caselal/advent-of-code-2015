5f0386de8002bc4b90fdc23e3646650204212745
Cassandra Luong <cassandraluong@gmail.com>
2024-08-30 22:19:11 -0700

    Complete AoC Day 16

--- 16-aunt-sue/aunt_sue.py
+++ 16-aunt-sue/aunt_sue.py
@@ -2,9 +2,6 @@
 Advent of Code - Day 16: Aunt Sue
 """
 
-###########
-# Part One
-###########
 # load data file
 with open("input.txt", "r") as input:
     puzzle_input = input.read()
@@ -38,26 +35,54 @@ def parse_information(input_string):
     return aunt_number, aunt_information
 
 
-aunt_sue = ""
-compound_count = 0
+###########
+# Part One
+###########
+
 for string in puzzle_input:
-    count = 0
     aunt_number, aunt_information = parse_information(string)
 
     aunt_compounds_set = set(aunt_information.keys())
     common_compounds = sample_compounds_set.intersection(aunt_compounds_set)
 
-    for compound in common_compounds:
-        if ticker_tape.get(compound) == aunt_information.get(compound):
-            count += 1
+    if all(
+        ticker_tape.get(compound) == aunt_information.get(compound)
+        for compound in common_compounds
+    ):
+        break
 
-    if count > compound_count:
-        aunt_sue = aunt_number
-        compound_count = count
+print(aunt_number)
 
-
-print(aunt_sue)
 
 ###########
 # Part Two
 ###########
+# function that compares compound values from the sample to a specific aunt sue
+def compare_compounds(common_compound, sample_dict, aunt_dict):
+    compound_value_aunt = aunt_dict.get(common_compound)
+    compound_value_sample = sample_dict.get(common_compound)
+
+    match common_compound:
+        case "cats" | "trees":
+            return compound_value_aunt > compound_value_sample
+
+        case "pomeranians" | "goldfish":
+            return compound_value_aunt < compound_value_sample
+
+        case _:
+            return compound_value_aunt == compound_value_sample
+
+
+for string in puzzle_input:
+    aunt_number, aunt_information = parse_information(string)
+
+    aunt_compounds_set = set(aunt_information.keys())
+    common_compounds = sample_compounds_set.intersection(aunt_compounds_set)
+
+    if all(
+        compare_compounds(compound, ticker_tape, aunt_information)
+        for compound in common_compounds
+    ):
+        break
+
+print(aunt_number)
